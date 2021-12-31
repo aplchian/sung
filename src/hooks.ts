b4e435316773f0d11d3f76bf3ad0df7f181b8f2b
aplchian <alex.boquis@gmail.com>
2021-12-30 19:40:57 -0500

    fix cookie error

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,7 @@
 import cookie from 'cookie';
 
 export async function handle({ request, resolve }) {
-	const cookies = cookie.parse(request.headers.cookie || { user: null });
+	const cookies = cookie.parse(request.headers.cookie || '');
 	request.locals.user = cookies.user;
 	const response = await resolve(request);
 	response.headers['set-cookie'] = `user=${request.locals.user || ''}; path=/; HttpOnly`;
